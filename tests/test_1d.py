81d8439a3490b4f67de1ce3f7470957725e841d3
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-11-03 00:32:15 +0000

    some changes

--- tests/test_1d.py
+++ tests/test_1d.py
@@ -5,10 +5,10 @@ from nldesa import EquationSystem
 
 
 # Define the system of equations
-def pendulum(y_val, _, a_val):
+def pendulum(y, t, a):
     """Equation system of the pendulum."""
-    theta, omega = y_val
-    dydt = jnp.asarray([omega, a_val[0] + a_val[1]*jnp.sin(theta)])
+    theta, omega = y
+    dydt = jnp.asarray([omega, a[0] + a[1]*jnp.sin(theta)])
     return dydt
 
 
